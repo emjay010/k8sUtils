25f05f3f0023162c4581fa1b3c076c3d4bfc8271
root <root@C-5CG146B2LX.localdomain>
2025-08-07 12:49:18 +0530

    Updated set-alias.sh
    Added features:
    1. Dynamically adds new aliases.
    2. List aliases.
    3. Help option

--- set-alias.sh
+++ set-alias.sh
@@ -1,78 +1,81 @@
 #!/bin/bash
 
-# Function to set Kubernetes namespace
-set_kube_namespace() {
-  export KUBE_NAMESPACE=$1
-}
+ALIAS_FILE="/tmp/kube_aliases.sh"
+DESCRIPTION_FILE="/tmp/kube_alias_descriptions.txt"
 
-# Function to set Helm namespace
-set_helm_namespace() {
-  export HELM_NAMESPACE=$1
-}
-
-# Check if both namespace arguments are provided
-if [ -z "$1" ] ; then
-  echo "Usage: $0 <k8s-namespace> [-n <another-namespace>] "
-  exit 1
+# Ensure alias file is sourced
+if [ -f "$ALIAS_FILE" ]; then
+  . "$ALIAS_FILE"
 fi
 
-# Set the Kubernetes and Helm namespaces
-set_kube_namespace "$1"
-set_helm_namespace "$1"
+# Function to print help
+show_help() {
+  cat <<EOF
 
-# Parse the command-line options
-while getopts ":n:" opt; do
-  case ${opt} in
-    n )
-      another_namespace=$OPTARG
-      ;;
-    \? )
-      echo "Invalid option: $OPTARG" 1>&2
-      exit 1
-      ;;
-    : )
-      echo "Invalid option: $OPTARG requires an argument" 1>&2
-      exit 1
-      ;;
-  esac
-done
-shift $((OPTIND -1))
+Usage:
+  . set-alias.sh --n <ns>        # Set or switch current namespace
+  . set-alias.sh --l                      # List current aliases and their descriptions
+  . set-alias.sh --add <alias> "<command>" "<description>"  # Add custom alias (auto namespace-aware if kubectl/helm)
+  . set-alias.sh --help                  # Show this help message
 
-# Set namespace according to provided option or default namespace
-if [ -n "$another_namespace" ]; then
-  KUBE_NAMESPACE=$another_namespace
-  HELM_NAMESPACE=$another_namespace
-fi
+Examples:
+  . set-alias.sh --n osns0011
+  . set-alias.sh --add kgloy "kubectl get leases -o yaml" "Get leases in yaml"
+  . set-alias.sh --l
 
-# Kubernetes aliases with dynamic namespace
-alias k='kubectl'
-alias kgp='kubectl get pods --namespace=${KUBE_NAMESPACE:-default}'
-alias kg='kubectl get --namespace=${KUBE_NAMESPACE:-default}'
-alias kgs='kubectl get services --namespace=${KUBE_NAMESPACE:-default}'
-alias kgd='kubectl get deployments --namespace=${KUBE_NAMESPACE:-default}'
-alias kga='kubectl get all --namespace=${KUBE_NAMESPACE:-default}'
-alias kd='kubectl describe --namespace=${KUBE_NAMESPACE:-default}'
-alias kc='kubectl create --namespace=${KUBE_NAMESPACE:-default}'
-alias krm='kubectl delete --namespace=${KUBE_NAMESPACE:-default}'
-alias kaf='kubectl apply -f --namespace=${KUBE_NAMESPACE:-default}'
-alias kpf='kubectl port-forward --namespace=${KUBE_NAMESPACE:-default}'
-alias kl='kubectl logs --namespace=${KUBE_NAMESPACE:-default}'
-alias kex='kubectl exec -it --namespace=${KUBE_NAMESPACE:-default}'
-alias kedit='kubectl edit --namespace=${KUBE_NAMESPACE:-default}'
-alias kcp='kubectl cp --namespace=${KUBE_NAMESPACE:-default}'
-alias ktop='kubectl top pods --namespace=${KUBE_NAMESPACE:-default}'
-alias kgl='kubectl get leases --namespace=${KUBE_NAMESPACE:-default}'
-alias kgnad='kubectl get net-attach-def --namespace=${KUBE_NAMESPACE:-default}'
+EOF
+}
 
-# Helm aliases with dynamic namespace
-alias h='helm3'
-alias hi='helm3 install --namespace=${HELM_NAMESPACE:-default}'
-alias hupg='helm3 upgrade --namespace=${HELM_NAMESPACE:-default}'
-alias hls='helm3 ls --namespace=${HELM_NAMESPACE:-default}'
-alias hrm='helm3 delete --namespace=${HELM_NAMESPACE:-default}'
-alias hrb='helm3 rollback --namespace=${HELM_NAMESPACE:-default}'
-alias hgv='helm3 get values --namespace=${HELM_NAMESPACE:-default}'
-alias hgm='helm3 get manifest --namespace=${HELM_NAMESPACE:-default}'
-alias hh='helm3 history --namespace=${HELM_NAMESPACE:-default}'
+# Function to make command namespace-aware
+namespace_wrap() {
+  local cmd="$1"
+  if [[ "$cmd" == kubectl* ]]; then
+    echo "$cmd --namespace=\${KUBE_NAMESPACE:-default}"
+  elif [[ "$cmd" == helm3* ]]; then
+    echo "$cmd --namespace=\${HELM_NAMESPACE:-default}"
+  else
+    echo "$cmd"
+  fi
+}
 
-echo "Aliases set for Kubernetes namespace: ${KUBE_NAMESPACE:-default} and Helm namespace: ${HELM_NAMESPACE:-default}"
+# Process arguments
+case "$1" in
+  --n)
+    shift
+    export KUBE_NAMESPACE="$1"
+    export HELM_NAMESPACE="$1"
+    echo "Switched to namespace: $1"
+    ;;
+  --add)
+    shift
+    alias_name="$1"
+    shift
+    raw_command="$1"
+    shift
+    description="$1"
+
+    # Add namespace awareness
+    cmd_with_ns=$(namespace_wrap "$raw_command")
+
+    echo "alias $alias_name='$cmd_with_ns'" >> "$ALIAS_FILE"
+    echo "$alias_name - $description" >> "$DESCRIPTION_FILE"
+    . "$ALIAS_FILE"
+    echo "Alias '$alias_name' added."
+    ;;
+  --l)
+    echo "=== Current Aliases ==="
+    alias
+    echo
+    if [ -f "$DESCRIPTION_FILE" ]; then
+      echo "=== Descriptions (custom added) ==="
+      cat "$DESCRIPTION_FILE"
+    fi
+    ;;
+  --help)
+    show_help
+    ;;
+  *)
+    show_help
+    ;;
+esac
+
